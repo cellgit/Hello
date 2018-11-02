fa3edf323eb9077df022803d60bdaa8e7f2ad780
kokora <liuhongli2120@gmail.com>
2018-11-02 21:52:01 +0800

    eee

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,12 +1,10 @@
 import FluentSQLite
 import Vapor
-import FluentPostgreSQL
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     /// Register providers first
-//    try services.register(FluentSQLiteProvider())
-    try services.register(FluentPostgreSQLProvider())
+    try services.register(FluentSQLiteProvider())
 
     /// Register routes to the router
     let router = EngineRouter.default()
@@ -20,25 +18,16 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     services.register(middlewares)
 
     // Configure a SQLite database
-//    let sqlite = try SQLiteDatabase(storage: .memory)
+    let sqlite = try SQLiteDatabase(storage: .memory)
 
     /// Register the configured SQLite database to the database config.
     var databases = DatabasesConfig()
-    
-    let databaseConfig = PostgreSQLDatabaseConfig(hostname: "localhost",
-                                              username: "postgres",
-                                              database: "kokora")
-    
-    let database = PostgreSQLDatabase(config: databaseConfig)
-    databases.add(database: database, as: .psql)
-//    databases.add(database: sqlite, as: .sqlite)
+    databases.add(database: sqlite, as: .sqlite)
     services.register(databases)
 
     /// Configure migrations
     var migrations = MigrationConfig()
-//    migrations.add(model: Todo.self, database: .sqlite)
-    migrations.add(model: HotKeyModel.self, database: .psql)
-    migrations.add(migration: AddingDescriptionToHotKeys.self, database: .psql)
+    migrations.add(model: Todo.self, database: .sqlite)
     services.register(migrations)
 
 }
